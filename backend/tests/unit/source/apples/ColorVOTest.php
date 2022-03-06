07ca8d2d0aab5d58cfd38d71c69a82ebbf76efd9
Pantagruel74 <tostar74@mail.ru>
2022-03-06 23:52:18 +0500

    написал тесты инициализации и валидации для ColorVOTest.php

--- backend/tests/unit/source/apples/ColorVOTest.php
+++ backend/tests/unit/source/apples/ColorVOTest.php
@@ -3,21 +3,41 @@
 namespace unit\source\apples;
 
 use backend\source\apples\AppleColorVO;
+use yii\base\ErrorException;
+use yii\base\InvalidArgumentException;
 
 class ColorVOTest extends \Codeception\Test\Unit
 {
     public function testInitAndValidation()
     {
-        $goodConfig1 = [
+        $appleByGoodConfig1 = AppleColorVO::createAndValidateStrictly([
             AppleColorVO::_rDec => 11,
             AppleColorVO::_bDec => 255,
             AppleColorVO::_gDec => 15,
-        ];
-
-        $appleByGoodConfig1 = AppleColorVO::createAndValidateStrictly($goodConfig1);
+        ]);
 
         $this->assertEquals($appleByGoodConfig1->rDec, 11);
         $this->assertEquals($appleByGoodConfig1->bDec, 255);
         $this->assertEquals($appleByGoodConfig1->gDec, 15);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleByBadConfig1 = AppleColorVO::createAndValidateStrictly([
+            AppleColorVO::_rDec => 11,
+            AppleColorVO::_gDec => 15,
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleByBadConfig2 = AppleColorVO::createAndValidateStrictly([
+            AppleColorVO::_rDec => -11,
+            AppleColorVO::_gDec => 155,
+            AppleColorVO::_bDec => 0,
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleByBadConfig3 = AppleColorVO::createAndValidateStrictly([
+            AppleColorVO::_rDec => 11,
+            AppleColorVO::_gDec => 155,
+            AppleColorVO::_bDec => 258,
+        ]);
     }
 }

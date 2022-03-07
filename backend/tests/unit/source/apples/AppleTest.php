2d12042a4fe2922a8a59ca70615ecb1e0f07d5c1
Pantagruel74 <tostar74@mail.ru>
2022-03-08 01:12:12 +0500

    Написал тест инициализации для яблока.

--- backend/tests/unit/source/apples/AppleTest.php
+++ backend/tests/unit/source/apples/AppleTest.php
@@ -2,16 +2,169 @@
 
 namespace unit\source\apples;
 
+use backend\source\apples\AppleColorVO;
 use backend\source\apples\AppleEn;
+use backend\source\apples\AppleStatusVO;
 
 class AppleTest extends \Codeception\Test\Unit
 {
     public function testInitializationAndValidation()
     {
-//        $apple = AppleEn::createAndValidateStrictly([]);
+        $time = time();
+        $appleGoodConfigured = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33f68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+        $this->assertEquals($appleGoodConfigured->getStatusCode(), AppleStatusVO::STATUS_ON_THE_TREE);
+        $this->assertEquals($appleGoodConfigured->falledAt, null);
+        $this->assertEquals($appleGoodConfigured->createdAt, $time);
+        $this->assertEquals($appleGoodConfigured->eatenPercent, 0);
+        $this->assertEquals($appleGoodConfigured->color->getHex(), '33f68a');
 
-//        $goodCreationConfig = [
-//
-//        ];
+        $appleGoodConfigured2 = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33f68a'),
+            AppleEn::_falledAt => $time,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 20,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ROTTEN,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithoutColor = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => null,
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidColor = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33g68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidFallenAtProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => 'aa',
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidFallenAtAndStatusPropsCombination = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ROTTEN,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidCreatedAtProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => null,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidCreatedAtProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => 'aa',
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidEatenPercentProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => -10,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidEatenPercentProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 102,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidEatenPercentProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 'aaf',
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidEatenPercentAndStatusPropsCombination = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 15,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => AppleStatusVO::STATUS_ON_THE_TREE,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidStatusProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => AppleStatusVO::createAndValidateStrictly([
+                AppleStatusVO::_statusCode => 7,
+            ])
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $appleWithInvalidStatusProp = AppleEn::createAndValidateStrictly([
+            AppleEn::_color => AppleColorVO::createByHexAndValidateStrictly('33c68a'),
+            AppleEn::_falledAt => null,
+            AppleEn::_createdAt => $time,
+            AppleEn::_eatenPercent => 0,
+            AppleEn::_status => 'aa'
+        ]);
+
     }
 }
